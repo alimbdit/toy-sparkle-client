61d06c731125e680c4d16765223521374d4493cc
alimbdit <alimbdit@gmail.com>
2023-05-18 22:37:59 +0600

    environment variables added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAEIfqpjORtL2V-BYvLm_oZf9GhNsk4OkU",
-  authDomain: "toy-sparkle-client.firebaseapp.com",
-  projectId: "toy-sparkle-client",
-  storageBucket: "toy-sparkle-client.appspot.com",
-  messagingSenderId: "737482754074",
-  appId: "1:737482754074:web:7ac80a285735da6f4069a3"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
